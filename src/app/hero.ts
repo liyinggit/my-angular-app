21db64c3fa28e8f450f82ceeaae03b32e2fd5b09
liyinggit <2501830002@qq.com>
2019-04-21 17:49:18 +0800

    1、英雄编辑器

---
+++ src/app/hero.ts
@@ -0,0 +1,8 @@
+/**
+ * 创建的hero类
+ */
+
+export class Hero {
+    id:number;
+    name:string;
+}
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero'
 
 @Component({
-  selector: 'app-heroes',
-  templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  selector: 'app-heroes',   //组件的选择器（css元素选择器）
+  templateUrl: './heroes.component.html',    //组件模板元素的位置
+  styleUrls: ['./heroes.component.css']    //组件私有css样式表文件位置
 })
 export class HeroesComponent implements OnInit {
+  //添加一个hero属性，可以在模板文件heroes.component.html中使用
+  hero :Hero = {
+    id:1,
+    name:'windstorm'
+  }
 
   constructor() { }
 
+  /**
+   * 生命周期钩子，初始化逻辑
+   */
   ngOnInit() {
   }
 
+  
 }
