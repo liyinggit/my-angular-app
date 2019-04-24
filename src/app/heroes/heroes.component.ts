97c0b7d9cdf8536f5b3cde2264db1b7dc99891b1
liyinggit <2501830002@qq.com>
2019-04-24 10:01:56 +0800

    英雄编辑器

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {Hero} from '../hero';
 
 @Component({
-  selector: 'app-heroes',
+  selector: 'app-heroes',   //组件的选择器，css元素选择器
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+
+ hero:Hero ={
+   id:1,
+   name:'windstorm'
+ }
 
   constructor() { }
 
