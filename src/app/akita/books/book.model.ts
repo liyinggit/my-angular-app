8e4ba70ff0c7bf0638b9ed5b35831793631887a1
liyinggit <2501830002@qq.com>
2019-04-26 16:00:32 +0800

    husky

---
+++ src/app/akita/books/book.model.ts
@@ -0,0 +1,11 @@
+import { ID } from '@datorama/akita';
+
+export interface Book {
+  id: ID;
+}
+
+export function createBook(params: Partial<Book>) {
+  return {
+
+  } as Book;
+}
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
+import { enableAkitaProdMode } from '@datorama/akita';
 
 if (environment.production) {
   enableProdMode();
+  enableAkitaProdMode();  //在开发模式下设置，一遍正常使用store
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
